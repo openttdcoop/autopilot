02e4da200b805b3a33c1a9c7d3d6df45b205b8a1
dih <dih@localhost>
2008-08-01 17:10:05 +0200

    (svn r323) -Fix: forgot to change names of commands in the custom trains script

--- autopilot/scripts/irc/trains.tcl
+++ autopilot/scripts/irc/trains.tcl
@@ -1,8 +1,8 @@
 if {[::mod_irc::nickIsOp [who]]} {
 	variable numtrains [lindex $bang_command 1]
-	exp_send -i $::ds "patch max_trains $numtrains\r"
+	::ap::game::console "patch max_trains $numtrains\r"
 	# publicly announce the config changes
-	say_everywhere "[who] set max_trains to $numtrains"
+	::ap::say::everywhere "[who] set max_trains to $numtrains"
 } else {
 	$replyto "[who]: you must be operator for this command"
 }
