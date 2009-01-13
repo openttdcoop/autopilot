aaceb32a76e84db9cd75ee717482818b39084f30
Osai <Osai@localhost>
2009-01-13 10:16:42 +0100

    (svn r531) Fix: Fixed a typo.

--- autopilot/scripts/callback/on_game_join.tcl
+++ autopilot/scripts/callback/on_game_join.tcl
@@ -1,5 +1,5 @@
 say::private "Welcome [who]"
-say::private "You are plying on \"[::ap::config::get network server_name]\""
+say::private "You are playing on \"[::ap::config::get network server_name]\""
 say::private [::ap::func::map_strings "House rules are at URL"]
 say::private "say !help for more information"
 say::private "---"
